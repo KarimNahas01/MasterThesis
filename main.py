bdf3f722b96baa1edeb7d3e0ffcd726b578203fd
KarimNahas01 <karimnah01@hotmail.com>
2024-02-06 10:25:39 +0100

    Established a connection

--- main.py
+++ main.py
@@ -3,10 +3,61 @@ import numpy as np
 import cv2
 import time
 import os
+import socket
+import math
+import struct
+import random
 
 import calibration_tools as ct
 # from box_dimensioner_multicam.box_dimensioner_multicam_demo import run_calibration#, calculate_values, visualise_measurements
 # from box_dimensioner_multicam.measurement_task import visualise_measurements as visualise_measurements_multiple
+
+def move_J(target_position, a=0.3, v=0.5, robot_ip="192.168.2.177", robot_port=30002):
+
+    target_position = [pos*math.pi/180 for pos in target_position]
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((robot_ip, robot_port))
+
+    move_command = f'movej({target_position}, a={a}, v={v})\n'
+    sock.sendall(move_command.encode())
+
+    # wait_command = 'wait until get_actual_joint_positions() == target_position\n'
+    # wait_command = 'ur_script_command_wait_1 = "wait until movej_done()\n'
+    # sock.sendall(wait_command.encode())
+
+    sock.close()
+
+def move_to_cartesian_coordinate(target_coordinate, robot_ip="192.168.2.177", robot_port=30002):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((robot_ip, robot_port))
+
+        cartesian_coordinate = convert_to_cartesian_pose(target_coordinate[:3], target_coordinate[3:])
+
+        ur_script_command = f'movel({cartesian_coordinate}, a=0.3, v=0.5)\n'
+        sock.sendall(ur_script_command.encode())
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        sock.close()
+
+def convert_to_cartesian_pose(xyz, rpy):
+    
+    qw = math.cos(rpy[0] / 2) * math.cos(rpy[1] / 2) * math.cos(rpy[2] / 2) + \
+         math.sin(rpy[0] / 2) * math.sin(rpy[1] / 2) * math.sin(rpy[2] / 2)
+    qx = math.sin(rpy[0] / 2) * math.cos(rpy[1] / 2) * math.cos(rpy[2] / 2) - \
+         math.cos(rpy[0] / 2) * math.sin(rpy[1] / 2) * math.sin(rpy[2] / 2)
+    qy = math.cos(rpy[0] / 2) * math.sin(rpy[1] / 2) * math.cos(rpy[2] / 2) + \
+         math.sin(rpy[0] / 2) * math.cos(rpy[1] / 2) * math.sin(rpy[2] / 2)
+    qz = math.cos(rpy[0] / 2) * math.cos(rpy[1] / 2) * math.sin(rpy[2] / 2) - \
+         math.sin(rpy[0] / 2) * math.sin(rpy[1] / 2) * math.cos(rpy[2] / 2)
+
+    cartesian_pose = f'p[{xyz[0]}, {xyz[1]}, {xyz[2]}, {qx}, {qy}, {qz}, {qw}]'
+
+    return cartesian_pose
 
 def main():
 
@@ -22,6 +73,37 @@ def main():
     align_to = rs.stream.color # ----
     align = rs.align(align_to) # ----
 
+    robot_ip = "192.168.2.177"
+    robot_port = 30002
+
+    # home_pos = f'[0, {-math.pi/2}, 0, {-math.pi/2}, 0, 0]'
+    home_pos = [0, -90, 0, -90, 0, 0]
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((robot_ip, robot_port))
+
+    subroutine_name = "Test_Move"
+
+    random_pos = [180, 45, -90, 90, 0, 180]
+
+    joint_limits = [[-360, 360], [-220, 40], [-145, 145], [-360, 360], [-360, 360], [-360, 360]]
+
+    # move_J([0, -90, -0, 360, 90, 0])
+    # random_pos = [random.uniform(limit[0], limit[1]) for limit in joint_limits]
+    # move_J(home_pos)
+    # Example usage
+    # target_coordinate = [500, 200, -280, 1.237, -2.697, 1.623]
+    # move_to_cartesian_coordinate(target_coordinate)
+    move_J(random_pos)
+    time.sleep(10)
+    move_J(home_pos)
+    # for _ in range(40):
+    #     move_J(random_pos)
+    #     time.sleep(10)
+    #     move_J(home_pos)
+    #     time.sleep(10)
+
+    exit()
     # pipe.start(config)
 
     # Start the RealSense pipeline
@@ -37,10 +119,10 @@ def main():
 
     # Perform your measurement and display logic here
 
-    n_imgs = 5
+    n_imgs = int(input("Input number of poses: "))# 5
     curr_img = 1
     images_folder_name = 'img'
-    connector = "robot_testing_2" # input('Input name of connector: ').replace(" ", "_")
+    connector = input('\nInput name of connector: ').replace(" ", "_")
     message_sent = False
     folder_path = os.path.join(images_folder_name, f'connector_{connector}')
     timer = time.time()
